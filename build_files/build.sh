272e8edc77ba53a93bf915abc511f7a9db9c6571
Charles Joseph Christie II <charles.christie.ii@hey.com>
2026-09-02 00:04:26 -0400

    Begin work on build.sh
    `build.sh` is still currently incomplete. Required steps:
    
    * Set up vendordeps, run ToolsUpdater (and create .desktop files for
      them), deploy utilities (like advantagescope and elastic), set up
      systemcore, install vsix extensions, and bootstrap the gradle cache.
    
      See https://share.google/aimode/GZilzll7fiMKRODzo for details.

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,6 +7,69 @@ cp -avf "/ctx/system_files"/. /
 
 ### Install packages
 
+#### Versions
+WPILIB_DIR="/ctx/build_files/wpilib_context"
+WPILIB_INSTALL_DIR="/usr/local/wpilib/2027"
+GRADLE_CACHE_DIR="/usr/share/gradle/cache"
+VSCODE_TEMP_DIR="/tmp/vscode-root"
+VSCOODE_EXTENSIONS_DIR="/usr/share/code/extensions"
+VSCODE_EXTENSIONS=(
+    "MS-vsliveshare.vsliveshare"
+    "vscjava.vscode-java-pack"
+    "christian-kohler.path-intellisense"
+    "EditorConfig.EditorConfig"
+    "maxking.forgejo-vscode"
+    "usernamehw.errorlens"
+    "DavidAnson.vscode-markdownlint"
+    "PKief.material-icon-theme"
+)
+
+# Install FRC and WPILib for robotics development
+
+echo "=== Installing WPILib Extensions into System Layer ==="
+mkdir -p ${WPILIB_INSTALL_DIR}
+cd ${WPILIB_DIR}
+
+echo "--- Installing JDK and Maven ---"
+
+mv jdk ${WPILIB_INSTALL_DIR}/jdk
+mv maven ${WPILIB_INSTALL_DIR}/maven
+
+echo "--- Setting up environment variables for WPILib ---"
+
+mkdir -p /etc/profile.d
+cat << 'EOF' > /etc/profile.d/wpilib.sh
+export JAVA_HOME="/usr/local/wpilib/2027/jdk"
+export PATH="${JAVA_HOME}/bin:${PATH}"
+export WPILIB_OFFLINE_MAVEN="/usr/local/wpilib/2027/maven"
+EOF
+
+# Source the profile to make the environment variables available in this script
+source /etc/profile.d/wpilib.sh
+
+echo "=== Installing other VSCode Extensions into System Layer ==="
+
+for extension in "${VSCODE_EXTENSIONS[@]}"; do
+    code --user-data-dir=${VSCODE_TEMP_DIR} --extensions-dir=${VSCOODE_EXTENSIONS_DIR} --install-extension ${extension}
+done
+
+# Vivaldi as default browser
+
+
+
+# Install gh command line tool from the official GitHub repository
+
+# Install GitHub Desktop
+
+# Install Bitwarden password manager
+
+# Install Discord Canary
+
+# Create website shortcut for onshape
+
+# Install Rider
+
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
